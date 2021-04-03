98e10e1e7602d0ca15d46f46e2f35c6192619fb4
Vera <vera.e.kovaleva@gmail.com>
2021-04-03 22:12:58 +0300

    - Наведен порядок в репозитории; - Добавлено решение для задачи урока 5. ;)

--- lesson_05/main.go
+++ lesson_05/main.go
@@ -3,34 +3,30 @@ package main
 import "fmt"
 
 func fibonacci(number int) (int, error) {
-
 	if number < 0 {
-		return 0, fmt.Errorf("number cannot be negative: %d", number)
+		return 0, fmt.Errorf("number cannot be <0 : %d", number)
 	}
 
 	cache := make(map[int]int)
 
-	return calcImpl(number, &cache), nil
+	return calculate(number, &cache), nil
 }
 
-func calcImpl(number int, cache *map[int]int) int {
+func calculate(number int, cache *map[int]int) int {
 	if number <= 1 {
 		return number
 	}
 
 	cachedValue, hasValue := (*cache)[number]
+
 	if !hasValue {
-		cachedValue = calcImpl(number-1, cache) + calcImpl(number-2, cache)
+		cachedValue = calculate(number-1, cache) + calculate(number-2, cache)
 		(*cache)[number] = cachedValue
 	}
-
 	return cachedValue
 }
 
 func main() {
-	fmt.Println(fibonacci(0))
-	fmt.Println(fibonacci(2))
-	fmt.Println(fibonacci(9))
 	fmt.Println(fibonacci(50))
 	fmt.Println(fibonacci(-1))
 }
